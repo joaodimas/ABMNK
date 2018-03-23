75836ace820367c7650c9b894417f2d74b82c3fd
joaodimas <joaohenriqueavila@gmail.com>
2018-03-23 13:29:35 +0100

    Empirical analysis

--- import_real_data.R
+++ import_real_data.R
@@ -6,88 +6,165 @@ rm(list = ls())
 setwd("/Users/jdimas/GitHub/ABMNK/real_data")
 Quandl.api_key("iyqies2dKE9mRZpR2zUm")
 
+# Select the series to import
+OUTPUT_GAP <- TRUE
+INFLATION <- TRUE
+INTEREST <- TRUE
+EXP_INFLATION <- TRUE
+BCB_GDP_DEFLATOR <- FALSE
+UNCERTAINTY <- TRUE
+QUARTER_DUMMIES <- TRUE
+IMF_FINANCIAL_STATS <- TRUE
+IBCBR <- TRUE
+
+# Select the first year in the series
+INITIAL_YEAR <- 1960
+
+
+date <- as.yearqtr(INITIAL_YEAR + seq(0, 235)/4)
+data <- data.frame(date, row.names="date")
+
 # Import Output Gap (IPEA)
-data <- read.csv("ipea-CC37_Produto-Potencial-trimestral-1993t1-2017t3.csv", sep=";", skip=5, header=FALSE, col.names=c("date", "pot_gdp", "real_gdp", "output_gap", "X1", "X2", "X3", "X4"))
-data <- data[,c("date","output_gap")]
-data$date <- as.yearqtr(data$date, format="%Y T%q")
-rownames(data) <- data$date
-data$output_gap <- as.numeric(gsub('%', '', data$output_gap))/100
+if (OUTPUT_GAP) {
+  outputGap <- read.csv("ipea-CC37_Produto-Potencial-trimestral-1993t1-2017t3.csv", sep=";", skip=5, header=FALSE, col.names=c("date", "pot_gdp", "real_gdp", "output_gap", "X1", "X2", "X3", "X4"))
+  outputGap <- outputGap[,c("date","output_gap")]
+  outputGap$date <- as.yearqtr(outputGap$date, format="%Y T%q")
+  rownames(outputGap) <- outputGap$date
+  outputGap$output_gap <- as.numeric(gsub('%', '', outputGap$output_gap))/100
+  data <- merge(data, outputGap, by="row.names", all=TRUE)
+  rm(outputGap)
+  rownames(data) <- data$Row.names
+  data$date <- rownames(data)
+  data$Row.names <- NULL
+}
 
 # Import inflation (Central Bank of Brazil, IPCA)
 # We download monthly inflation, aggregate by quarter, and then we annualize.
 # As a result we have annualized quarterly inflation.
-inflation <- Quandl("BCB/433")
-inflation$Date <- as.yearqtr(inflation$Date)
-# Aggregate by quarter
-inflation <- aggregate(inflation$Value/100+1, by=list(inflation$Date), "prod")
-# Annualize
-inflation$inflation <- (inflation$x^4)-1
-rownames(inflation) <- inflation$Group.1
-inflation$Group.1 <- NULL
-inflation$x <- NULL
-data <- merge(data, inflation, by="row.names", all=TRUE)
-rm(inflation)
-rownames(data) <- data$Row.names
-data$date <- rownames(data)
-data$Row.names <- NULL
+if (INFLATION) {
+  inflation <- Quandl("BCB/433")
+  inflation$Date <- as.yearqtr(inflation$Date)
+  # Aggregate by quarter
+  inflation <- aggregate(inflation$Value/100+1, by=list(inflation$Date), "prod")
+  # Annualize
+  inflation$inflation <- (inflation$x^4)-1
+  rownames(inflation) <- inflation$Group.1
+  inflation$Group.1 <- NULL
+  inflation$x <- NULL
+  data <- merge(data, inflation, by="row.names", all=TRUE)
+  rm(inflation)
+  rownames(data) <- data$Row.names
+  data$date <- rownames(data)
+  data$Row.names <- NULL
+}
 
 # Import interest rate (Central Bank of Brazil, SELIC)
 # The interest rate is computed daily and represents the annual rate.
 # We collapse by quarter and take the current annual interest rate at the end of each quarter.
-interest <- data.frame(Quandl("BCB/1178", collapse="quarterly", type="zoo"))
-colnames(interest) <- "interest"
-data <- merge(data, interest, by="row.names", all=TRUE)
-rm(interest)
-rownames(data) <- data$Row.names
-data$date <- rownames(data)
-data$Row.names <- NULL
-
+if (INTEREST) {
+  interest <- data.frame(Quandl("BCB/1178", collapse="quarterly", type="zoo"))
+  colnames(interest) <- "nominal_interest_rate"
+  interest$nominal_interest_rate <- interest$nominal_interest_rate/100
+  data <- merge(data, interest, by="row.names", all=TRUE)
+  rm(interest)
+  rownames(data) <- data$Row.names
+  data$date <- rownames(data)
+  data$Row.names <- NULL
+}
 # Import expected inflation (Central Bank of Brazil, Expected inflation)
 # The expected inflation is computed daily and represents the expected inflation for the next 12 months.
 # We take the last value for each quarter.
-expInfl <- read.csv("bcb-expected inflation-2001jan-2018fev.csv", colClasses=c("Date", "numeric"))
-expInfl$dateqtr <- as.yearqtr(expInfl$date)
-expInfl <- aggregate(expInfl$"exp_inflation", by=list(expInfl$dateqtr), "tail", n=1)
-rownames(expInfl) <- expInfl$Group.1
-expInfl$exp_inflation <- expInfl$x
-expInfl$Group.1 <- NULL
-expInfl$x <- NULL
-data <- merge(data, expInfl, by="row.names", all=TRUE)
-rm(expInfl)
-rownames(data) <- data$Row.names
-data$date <- rownames(data)
-data$Row.names <- NULL
+if (EXP_INFLATION) {
+  expInfl <- read.csv("bcb-expected inflation-2001jan-2018fev.csv", colClasses=c("Date", "numeric"))
+  expInfl$dateqtr <- as.yearqtr(expInfl$date)
+  expInfl <- aggregate(expInfl$"exp_inflation", by=list(expInfl$dateqtr), "tail", n=1)
+  rownames(expInfl) <- expInfl$Group.1
+  expInfl$exp_inflation <- expInfl$x/100
+  expInfl$Group.1 <- NULL
+  expInfl$x <- NULL
+  data <- merge(data, expInfl, by="row.names", all=TRUE)
+  rm(expInfl)
+  rownames(data) <- data$Row.names
+  data$date <- rownames(data)
+  data$Row.names <- NULL
+}
+
+# Calculate real interest rate
+if(INTEREST & EXP_INFLATION) {
+  data$real_interest_rate <- data$nominal_interest_rate - data$exp_inflation
+}
 
 # Import GDP deflator (Central Bank of Brazil, GDP deflator, yearly)
-gdpDeflator <- data.frame(Quandl("BCB/1211", collapse="quarterly", type="zoo"))
-colnames(gdpDeflator) <- "bcb_gdp_deflator"
-data <- merge(data, gdpDeflator, by="row.names", all=TRUE)
-rm(gdpDeflator)
-rownames(data) <- data$Row.names
-data$date <- rownames(data)
-data$Row.names <- NULL
-
-# Import GDP components (IMF International Financial Statistics (IFS), Brazil, from 2013Q1 to 2017Q3, http://data.imf.org/regular.aspx?key=61545852)
-gdpComp <- read.csv("International_Financial_Statistics.csv", skip=1, sep=";")
-gdpComp$date <- as.yearqtr(gdpComp$date, format="Q%q %Y")
-for (i in 2:12) {
-  gdpComp[,i] <- as.numeric(gsub(',', '', gdpComp[,i]))
-  if (i <= 9) {
-    gdpComp[,i] <- gdpComp[,i]*1000000
-  }
-  if (i == 11 | i == 12) {
-    gdpComp[,i] <- gdpComp[,i]/100
-  }
+if (BCB_GDP_DEFLATOR) {
+  gdpDeflator <- data.frame(Quandl("BCB/1211", collapse="quarterly", type="zoo"))
+  colnames(gdpDeflator) <- "bcb_gdp_deflator"
+  data <- merge(data, gdpDeflator, by="row.names", all=TRUE)
+  rm(gdpDeflator)
+  rownames(data) <- data$Row.names
+  data$date <- rownames(data)
+  data$Row.names <- NULL
 }
-rownames(gdpComp) <- gdpComp$date
-gdpComp$date <- NULL
-data <- merge(data, gdpComp, by="row.names", all=TRUE)
-rm(gdpComp)
-rownames(data) <- data$Row.names
-data$date <- rownames(data)
-data$Row.names <- NULL
 
+# Import index of uncertainty calculated by FGV (IIE-br) from 2000 to 2018. 
+# The index comes in monthly values. I keep the last month of each quarter.
+if(UNCERTAINTY) {
+  uncertainty <- read.csv("FGV-uncertainty-IIE-br-2000to2018.csv", skip=1, colClasses = c("character", "numeric"))
+  uncertainty$date <- as.yearqtr(uncertainty$date, format="%m/%Y")
+  uncertainty <- aggregate(uncertainty$uncertainty, by=list(uncertainty$date), "tail", n=1)
+  rownames(uncertainty) <- uncertainty$Group.1
+  uncertainty$uncertainty <- uncertainty$x
+  uncertainty$Group.1 <- NULL
+  uncertainty$x <- NULL
+  data <- merge(data, uncertainty, by="row.names", all=TRUE)
+  rm(uncertainty)
+  rownames(data) <- data$Row.names
+  data$date <- rownames(data)
+  data$Row.names <- NULL
+}
 
+if(IBCBR) {
+  ibcbr <- read.csv("bcb-IBC-br-monthly-2003to2017.csv", skip=1, colClasses = c("character", "numeric"))
+  ibcbr$date <- as.yearqtr(ibcbr$date, format="%d/%m/%Y")
+  ibcbr <- aggregate(ibcbr$ibcbr, by=list(ibcbr$date), "tail", n=1)
+  rownames(ibcbr) <- ibcbr$Group.1
+  ibcbr$ibcbr <- ibcbr$x
+  ibcbr$Group.1 <- NULL
+  ibcbr$x <- NULL
+  data <- merge(data, ibcbr, by="row.names", all=TRUE)
+  rm(ibcbr)
+  rownames(data) <- data$Row.names
+  data$date <- rownames(data)
+  data$Row.names <- NULL
+}
+
+# Import several variables (IMF International Financial Statistics (IFS), Brazil, from 1990Q1 to 2017Q4, http://data.imf.org/regular.aspx?key=61545852)
+if (IMF_FINANCIAL_STATS) {
+  imfFinStats <- read.csv("International_Financial_Statistics.csv", skip=1, sep=";")
+  imfFinStats$date <- as.yearqtr(imfFinStats$date, format="Q%q %Y")
+  for (i in 2:12) {
+    imfFinStats[,i] <- as.numeric(gsub(',', '', imfFinStats[,i]))
+    if (i <= 9) {
+      imfFinStats[,i] <- imfFinStats[,i]*1000000
+    }
+    if (i == 11 | i == 12) {
+      imfFinStats[,i] <- imfFinStats[,i]/100
+    }
+  }
+  rownames(imfFinStats) <- imfFinStats$date
+  imfFinStats$date <- NULL
+  data <- merge(data, imfFinStats, by="row.names", all=TRUE)
+  rm(imfFinStats)
+  rownames(data) <- data$Row.names
+  data$date <- rownames(data)
+  data$Row.names <- NULL
+}
+
+if(QUARTER_DUMMIES) {
+  for(q in 2:4) {
+    data[grep(paste0("Q",q), data$date, fixed=TRUE),paste0("q",q)] <- 1
+    data[-grep(paste0("Q",q), data$date, fixed=TRUE),paste0("q",q)] <- 0
+  } 
+}
 
 # Save a CSV consolidated with all variables
 data$date <- as.Date(as.yearqtr(as.character(data$date)))
--- real_data_analysis.R
+++ real_data_analysis.R
@@ -1,36 +1,85 @@
 while (!require("plotly")) install.packages("plotly")
 while (!require("tseries")) install.packages("tseries")
 while (!require("vars")) install.packages("vars")
+while (!require("strucchange")) install.packages("strucchange")
+while (!require("ecm")) install.packages("ecm")
 
+# Initial import ----------------------------------------------------------
 rm(list = ls())
 setwd("/Users/jdimas/GitHub/ABMNK/real_data")
+data <- read.csv("data.csv", colClasses=c("Date", rep("numeric", 21)))
 
-# TODO: add all new variables
-data <- read.csv("data.csv", colClasses=c("Date", "numeric", "numeric", "numeric", "numeric", "numeric"))
-data$real_interest_rate <- data$nominal_interest_rate - data$exp_inflation
-after1995 <- na.omit(data[data$date > as.Date("1995-09-30"),])
-# after1995$nominal_interest_rate = as.numeric(scale(after1995$nominal_interest_rate))
-# after1995$inflation = as.numeric(scale(after1995$inflation))
-# after1995$output_gap = as.numeric(scale(after1995$output_gap))
-# after1995$exp_inflation = as.numeric(scale(after1995$exp_inflation))
-# after1995$real_interest_rate = as.numeric(scale(after1995$real_interest_rate))
-plot_ly(after1995, x=~date, y = ~(inflation+1)^3-1, name = 'quarterly inflation', type="scatter", mode = 'lines+markers') %>%
-  add_trace(y = ~real_interest_rate, name = 'real yearly interest rate') %>%
-  add_trace(y = ~exp_inflation, name = 'expected inflation') %>%
-  add_trace(y = ~output_gap, name = 'output gap')
+# Monetary policy responding to expected inflation ------------------------
 
-plot_ly(after1995, x=~date, y=~inflation, type="scatter", mode="lines")
-print(paste("Inflation is stationary:", adf.test(after1995$inflation)$p.value <= 0.05))
+df <- na.omit(data[,c("date", "exp_inflation", "nominal_interest_rate", "real_interest_rate")])
+rownames(df) <- c(1:nrow(df))
+plot_ly(df, x=~date, y = ~exp_inflation, name = 'expected inflation', type="scatter", mode = 'lines+markers',  hoverinfo = 'text', text = ~paste(rownames(df), date, exp_inflation, sep="<br>")) %>%
+  add_trace(y = ~real_interest_rate, name = 'real interest rate') %>%
+  add_trace(y = ~nominal_interest_rate, name = 'nominal interest rate') 
 
-plot_ly(after1995, x=~date, y=~nominal_interest_rate, type="scatter", mode="lines")
-print(paste("Nominal interest rate is stationary:", adf.test(after1995$nominal_interest_rate)$p.value <= 0.05))
+# There's a clear change in how monetary policy responds to inflation starting in October 2011. 
+# We see that expected inflation is still high, but the Central Bank starts decreasing interest rates.
+# The 'strange' behavior lasts until April 2013, when the Central Bank starts a smooth tightening of monetary policy.
+# We also see a maintenance of a high nominal rate from Jan to July 2016 even after expected inflation started decreasing.
+# This caused an increase in real rates as inflation went down. Shouldn't the CB have reacted quickly?
 
-plot_ly(after1995, x=~date, y=~output_gap, type="scatter", mode="lines")
-print(paste("Output gap is stationary:", adf.test(after1995$output_gap)$p.value <= 0.05))
+# Exploratory: compare the R2 of different lags on interest_rate, both real and nominal.
+lm1 <- summary(lm(nominal_interest_rate ~ lagpad(exp_inflation,1), df))
+lm2 <- summary(lm(nominal_interest_rate ~ exp_inflation, df))
+lm3 <- summary(lm(real_interest_rate ~ lagpad(exp_inflation,1), df))
+lm4 <- summary(lm(real_interest_rate ~ exp_inflation, df))
+print(paste("lm1 R2: ",lm1$r.squared))
+print(paste("lm2 R2: ",lm2$r.squared))
+print(paste("lm3 R2: ",lm3$r.squared))
+print(paste("lm4 R2: ",lm4$r.squared))
+# Highest R2 is nominal_interest_rate ~ exp_inflation lagged.
+
+breakpoints(nominal_interest_rate ~ lagpad(exp_inflation,1), data=df)$breakpoints
+# Breakpoints at observation number: 18 28 41 54 
+breakpoints(real_interest_rate ~ lagpad(exp_inflation,1), data=df)$breakpoints
+# Breakpoints at observation number: 19 28 41 54 
+
+# Monetary policy x Output gap and expected inflation ---------
+
+df <- na.omit(data[,c("date", "nominal_interest_rate", "real_interest_rate", "output_gap", "exp_inflation", "inflation")])
+rownames(df) <- c(1:nrow(df))
+plot_ly(df, x=~date, y = ~nominal_interest_rate, name = 'nominal interest rate', type="scatter", mode = 'lines+markers',  hoverinfo = 'text', text = ~paste(rownames(df), date, output_gap, exp_inflation, sep="<br>")) %>%
+  add_trace(y = ~output_gap, name = 'output gap')  %>%
+  add_trace(y = ~exp_inflation, name = "exp inflation")
+
+# Monetary policy x Inflation ---------------------------------
+
+df <- tail(na.omit(data[,c("date","inflation", "nominal_interest_rate", "output_gap", "q2", "q3", "q4")]),88)
+rownames(df) <- c(1:nrow(df))
+lm1 <- lm(inflation ~ q2 + q3 + q4, df)
+summary(lm1)
+df$inflation_adj <- lm1$residuals
+plot_ly(df, x=~date, y = ~nominal_interest_rate, name = 'nominal interest rate', type="scatter", mode = 'lines+markers',  hoverinfo = 'text', text = ~paste(rownames(df), date, sep="<br>")) %>%
+  add_trace(y = ~inflation_adj, name = "inflation season. adj.")
 
 
-var <- VAR(after1995[,c("inflation", "output_gap", "nominal_interest_rate", "exp_inflation")], lag.max=4, ic="AIC")
-var <- VAR(after1995[,c("inflation", "output_gap", "nominal_interest_rate", "exp_inflation")], lag.max=4, ic="AIC")
+# Other analysis  ---------------------------------------------------------
+
+# CONTINUE TO ANALYSE WHAT DROVE INTEREST RATES. PAY ATTENTION TO THE BREAK AROUND 2012.
+df$inflation_diff <- c(0, diff(df$inflation))
+df$exp_inflation_diff <- c(0, diff(df$exp_inflation))
+df$nominal_interest_rate_diff <- c(0, diff(df$nominal_interest_rate))
+df$output_gap_diff <- c(0, diff(df$output_gap))
+
+plot_ly(df, x=~date, y=~inflation_diff, type="scatter", mode="lines")
+adf.test(df$inflation_diff)
+
+plot_ly(df, x=~date, y=~exp_inflation_diff, type="scatter", mode="lines")
+adf.test(df$exp_inflation_diff)
+
+plot_ly(df, x=~date, y=~nominal_interest_rate_diff, type="scatter", mode="lines")
+adf.test(df$nominal_interest_rate_diff)
+
+plot_ly(df, x=~date, y=~output_gap_diff, type="scatter", mode="lines")
+adf.test(df$output_gap_diff)
+
+
+var <- VAR(df[,c("inflation_diff", "output_gap_diff", "nominal_interest_rate_diff", "exp_inflation_diff")], lag.max=4, ic="AIC")
 summary(var)
 
 Amat = diag(4)
@@ -47,13 +96,13 @@ Amat[4,3] <- NA
 svar <- SVAR(var, estmethod = "direct", Amat=Amat, Bmat=NULL, max.iter=1000)
 summary(svar)
 svar
-interestrate_inflation <- irf(svar, impulse="nominal_interest_rate", response="inflation", n.ahead=100)
+interestrate_inflation <- irf(svar, impulse="nominal_interest_rate_diff", response="inflation_diff", n.ahead=100)
 plot(interestrate_inflation)
-outputgap_inflation  <- irf(svar, impulse="output_gap", response="nominal_interest_rate")
+outputgap_inflation  <- irf(svar, impulse="output_gap_diff", response="nominal_interest_rate_diff", n.ahead=100)
 plot(outputgap_inflation)
-inflation_interestrate <- irf(svar, impulse="inflation", response="nominal_interest_rate", n.ahead=100)
+inflation_interestrate <- irf(svar, impulse="inflation_diff", response="nominal_interest_rate_diff", n.ahead=100)
 plot(inflation_interestrate)
-exp_inflation_interestrate <- irf(svar, impulse="exp_inflation", response="nominal_interest_rate", n.ahead=100)
+exp_inflation_interestrate <- irf(svar, impulse="exp_inflation_diff", response="nominal_interest_rate_diff", n.ahead=100)
 plot(exp_inflation_interestrate)
-interestrate_exp_inflation <- irf(svar, impulse="nominal_interest_rate", response="exp_inflation", n.ahead=100)
+interestrate_exp_inflation <- irf(svar, impulse="nominal_interest_rate_diff", response="exp_inflation_diff", n.ahead=100)
 plot(interestrate_exp_inflation)
